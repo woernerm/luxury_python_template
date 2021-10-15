29e464593a8e6090af24fd774f553eec4c1219ad
Marcus Wörner <61889355+woernerm@users.noreply.github.com>
2021-10-15 22:43:29 +0200

    Create report example
    Created an example to showcase the template's reporting features.

--- src/pythontemplate/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-Template package that you may use to quickly start your python project.
-"""
--- tests/test_example.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase as _TestCase
-import src.pythontemplate.example_module as _example
-
-
-class MyExampleTestCase(_TestCase):
-    def test_example_function(self):
-        value = 42
-        self.assertEqual(_example.example_function(value), value)
