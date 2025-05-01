0d392263eeae405308aefb0e4faeeb13016854c3
Marcus Wörner <61889355+woernerm@users.noreply.github.com>
2025-05-01 12:22:11 +0200

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,11 +9,22 @@ GROUP_WORD_BOUNDARY = 2
 def to_sentence(snake_str: str) -> str:
     """Convert snake_case string to a sentence."""
     # Replace underscores with spaces and capitalize the first letter
-    return re.sub(RE_WORD_BOUNDARY, r"\1 \3", snake_str).capitalize()
+    sentence = (
+        re.sub(RE_WORD_BOUNDARY, r"\1 \3", snake_str)
+        .removeprefix("test")
+        .strip()
+        .capitalize()
+    )
+    return f"{sentence}."
+
 
 def get_requirement(item):
     """ Convert a test item to a requirement. """
-    return to_sentence(item.name) or item.instance.__doc__
+    return (
+        item.instance._testMethodDoc
+        or item.instance.__doc__ 
+        or to_sentence(item.name)
+    )
      
 def pytest_collection_modifyitems(session, config, items):
     packagemod = importlib.import_module("package", "..")
@@ -26,6 +37,7 @@ def pytest_collection_modifyitems(session, config, items):
     release = meta.get("version")
 
     mod_prefix = str(settings.TEST_DIR.relative_to(Path.cwd()))
+    Path(settings.REQUIREMENTS_FILE).parent.mkdir(parents=True, exist_ok=True)
 
     # Write requirements to json file
     with open(settings.REQUIREMENTS_FILE, "w") as f:
@@ -46,7 +58,8 @@ def pytest_collection_modifyitems(session, config, items):
                 .replace("test_", "").replace(".", " ").capitalize()
             )
             f.write(f"### {modname}\n")
-            for id_, item in enumerate(content):
+            for item in content:
+                _, _, pymod = item.location
                 text = get_requirement(item)            
-                f.write(f"#### ID: {id_+1}\n\n")
+                f.write(f"#### {pymod}\n\n")
                 f.write(f"{text}\n\n")         
